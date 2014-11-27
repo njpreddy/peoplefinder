247737c1a06f54710855b739c187bf22905989be
Demotive <mat@demotive.com>
2014-11-27 14:15:40 +0000

    Adding new_layout.js for simple interaction sketching

---
+++ app/assets/javascripts/new_layout/new_layout.js
@@ -0,0 +1,30 @@
+/*
+
+ ***** NOTE *****
+
+ This is a scratchpad.
+ Used to demo UI effects, not intended for production!
+ It's sketch code!
+
+*/
+
+$(function() {
+
+  // add / remove job role in new_person
+  $('#duplicatePanelTemplate').hide();
+
+  $('#duplicatePanelControl').on('click', function(e) {
+    e.preventDefault();
+    $this = $(this);
+    var $newPanel = $($('#duplicatePanelTemplate').html());
+    $newPanel.insertBefore($this);
+  });
+
+  $('.roles').on('click', '.remove-parent-panel', function(e) {
+    e.preventDefault();
+    var $target = $(this).parents('.panel-indent');
+    console.log($target);
+    $target.remove();
+  });
+
+});
